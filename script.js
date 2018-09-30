833507639927fe46282385cd4f98515db504baec
alexg622 <alex.gm62288@gmail.com>
2018-09-30 16:21:51 -0700

    Add row and square classes

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  grabPiece()
+  movePiece()
 })
 
-let pieceToMove;
-let divSquare;
+let highlightedSquare = null
 
-const grabPiece = () => {
-  let pieces = document.querySelectorAll(".piece")
-  pieces.forEach(piece => {
-    piece.addEventListener("click", (e) => {
-      pieceToMove = e.target
-      e.target.style.border = "2px solid yellow"
+const movePiece = () => {
+  let squares = document.querySelectorAll(".square")
+  squares.forEach(square => {
+    square.addEventListener("click", (e) => {
+      let target = e.target
+      highlightSquare(target, e)
+      
     })
   })
 }
 
-const movePiece = () => {
-  
+const highlightSquare = (target, e) => {
+  if(Array.from(target.classList).includes("piece")) target = e.target.parentElement
+  if(typeof target.childNodes[1] === "object") {
+    if(highlightedSquare !== target) {
+      target.childNodes[1].style.border = "4px solid yellow"
+      if(highlightedSquare) highlightedSquare.childNodes[1].style.border = ""
+      highlightedSquare = target
+    }
+  }
 }
